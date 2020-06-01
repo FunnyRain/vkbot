1f2fe7fcd68fd378c7aa542288c4959c59f1a761
bitnoob <54314427+bitnooob@users.noreply.github.com>
2020-06-01 15:47:03 +0300

    Refactoring Console.php

--- src/Console.php
+++ src/Console.php
@@ -2,10 +2,37 @@
 
 class Console {
 
-    public
-        $bot,
-        $foreground_colors = [],
-        $background_colors = [];
+    public $bot;
+    
+    public $foreground_colors = [
+        "black" => "0;30",
+        "dark_gray" => "1;30",
+        "light_gray" => "0;37",
+        "blue" => "0;34",
+        "light_blue" => "1;34",
+        "green" => "0;32",
+        "light_green" => "1;32",
+        "cyan" => "0;36",
+        "light_cyan" => "1;36",
+        "red" => "0;31",
+        "light_red" => "1;31",
+        "purple" => "0;35",
+        "light_purple" => "1;35",
+        "brown" => "0;33",
+        "yellow" => "1;33",
+        "white" => "1;37"
+    ];
+    
+    public $background_colors = [
+        "black" => "40",
+        "red" => "41",
+        "green" => "42",
+        "yellow" => "43",
+        "blue" => "44",
+        "magenta" => "45",
+        "cyan" => "46",
+        "light_gray" => "47",
+    ];
 
     /**
      * Console constructor.
@@ -13,34 +40,6 @@ class Console {
      */
     public function __construct(Control $bot) {
         $this->bot = $bot;
-        $this->foreground_colors = [
-            "black" => "0;30",
-            "dark_gray" => "1;30",
-            "light_gray" => "0;37",
-            "blue" => "0;34",
-            "light_blue" => "1;34",
-            "green" => "0;32",
-            "light_green" => "1;32",
-            "cyan" => "0;36",
-            "light_cyan" => "1;36",
-            "red" => "0;31",
-            "light_red" => "1;31",
-            "purple" => "0;35",
-            "light_purple" => "1;35",
-            "brown" => "0;33",
-            "yellow" => "1;33",
-            "white" => "1;37"
-        ];
-        $this->background_colors = [
-            "black" => "40",
-            "red" => "41",
-            "green" => "42",
-            "yellow" => "43",
-            "blue" => "44",
-            "magenta" => "45",
-            "cyan" => "46",
-            "light_gray" => "47",
-        ];
     }
 
     /**
@@ -49,7 +48,8 @@ class Console {
      */
     public function log(string $title = "", string $subtitle = null) {
         echo PHP_EOL . self::color("[Лог] > " . date("d.m.y, H:i:s") . "> ", "blue") . self::color($title, "white");
-        if (!is_null($subtitle)) echo PHP_EOL . "\t" . self::color($subtitle, "green");
+        if (isset($subtitle))
+            echo PHP_EOL . "\t" . self::color($subtitle, "green");
     }
 
     /**
@@ -58,7 +58,8 @@ class Console {
      */
     public function error(string $title = "", string $subtitle = null) {
         echo PHP_EOL . self::color("[Ошибка] > " . date("d.m.y, H:i:s") . "> ", "red") . self::color($title, "white");
-        if (!is_null($subtitle)) echo PHP_EOL . "\t" . self::color($subtitle, "green");
+        if (isset($subtitle))
+            echo PHP_EOL . "\t" . self::color($subtitle, "green");
     }
 
     /**
@@ -67,7 +68,8 @@ class Console {
      */
     public function warning(string $title = "", string $subtitle = null) {
         echo PHP_EOL . self::color("[Предупреждение] > " . date("d.m.y, H:i:s") . "> ", "yellow") . self::color($title, "white");
-        if (!is_null($subtitle)) echo PHP_EOL . "\t" . self::color($subtitle, "green");
+        if (isset($subtitle))
+            echo PHP_EOL . "\t" . self::color($subtitle, "green");
     }
 
     /**
@@ -77,7 +79,8 @@ class Console {
     public function debug(string $title = "", string $subtitle = null) {
         if ($this->bot->debug == 1) {
             echo PHP_EOL . self::color("[Дебаг] > " . date("d.m.y, H:i:s") . "> ", "purple") . self::color($title, "white");
-            if (!is_null($subtitle)) echo PHP_EOL . "\t" . self::color($subtitle, "light_purple");
+            if (isset($subtitle))
+                echo PHP_EOL . "\t" . self::color($subtitle, "light_purple");
         }
     }
 
