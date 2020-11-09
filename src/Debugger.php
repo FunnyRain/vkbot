a620d067455f227968bf6bddb85b67ca70fc903e
FunnyRain <terapevt3414334@gmail.com>
2020-11-09 15:27:28 +0300

    :pencil: Обновление документации

--- src/Debugger.php
+++ src/Debugger.php
@@ -14,13 +14,26 @@ class Debugger {
      */
     public function getStickerId(): string {
         $object = $this->object;
-        $sticker_ids = "Debugger Result: \n";
+        $sticker_id = "Debugger Result: \n";
         if (isset($object['attachments'])) {
             foreach ($object['attachments'] as $stickers) {
                 if ($stickers['type'] == 'sticker')
-                    $sticker_ids .= "* Sticker_id: " . $stickers['sticker']['sticker_id'] . PHP_EOL;
+                    $sticker_id .= "* Sticker_id: " . $stickers['sticker']['sticker_id'] . PHP_EOL;
             }
         }
-        return $sticker_ids;
+        return $sticker_id;
+    }
+
+    /**
+     * Выводит айди чата
+     * @return string   Айди чата
+     */
+    public function getPeerId(): string {
+        $object = $this->object;
+        $peer_id = "Debugger Result: \n";
+        if (isset($object['peer_id'])) {
+            $peer_id .= "* Peer_id: " . $object['peer_id'] . PHP_EOL;
+        }
+        return $peer_id;
     }
 }
--- src/Messages.php
+++ src/Messages.php
@@ -8,29 +8,6 @@ class Messages {
         $this->bot = $bot;
     }
 
-    /** 
-     * (
-    [date] => 1604851861
-    [from_id] => 439239695
-    [id] => 3098
-    [out] => 0
-    [peer_id] => 439239695
-    [text] => f
-    [conversation_message_id] => 1543
-    [fwd_messages] => Array
-        (
-        )
-
-    [important] => 
-    [random_id] => 0
-    [attachments] => Array
-        (
-        )
-
-    [is_hidden] => 
-)
-    */
-
     /**
      * Получение сообщения
      * @param array $object
@@ -71,5 +48,49 @@ class Messages {
             'peer_id' => ($peer_id == 1) ? $this->bot->vkdata['peer_id'] : $peer_id,
             'sticker_id' => $id
         ]);
+    }
+
+    /**
+     * Отправка сообщения
+     * @param string $text      Текст сообщения
+     * @param string|integer $peer_ids  Айди получател(я/ей) (Перечислять через запятую)
+     * @param array $args       Дополнительные параметры
+     * @return array            Вернёт айди сообщения
+     */
+    public function sendMessage(string $text, $peer_ids, array $args = []): array {
+        if (!isset($text) and !isset($args['attachment']))
+            return $this->bot->getLog()->error('Не указан текст!');
+        if (preg_match('/(fname|lname|afname|alname|fullname|afullname)/ui', $text))
+            $text = $this->replaceNameToMessage(isset($args['from_id']) ? $args['from_id'] : $this->bot->vkdata['from_id'], $text);
+
+        return $this->bot->api('messages.send', [
+            'random_id' => rand(),
+            'peer_ids' => isset($peer_ids) ? $peer_ids : $this->bot->vkdata['peer_id'],
+            'message' => $text
+        ] + $args);
+    }
+
+    /**
+     * Заменяет ключевые слова на Имя\Фамилию пользователя
+     * @param integer $user_id  Айди пользователя
+     * @param string $text      Текст с ключевыми словами
+     * @return string           Готовый текст
+     */
+    public function replaceNameToMessage(int $user_id, string $text): string {
+        return str_replace(
+            ["{fname}", "{lname}", "{afname}", "{alname}", "{fullname}", "{afullname}"],
+            [
+                $this->getInfo($user_id)['first_name'],
+                $this->getInfo($user_id)['last_name'],
+                "[id{$user_id}|" . $this->getInfo($user_id)['first_name'] . "]",
+                "[id{$user_id}|" . $this->getInfo($user_id)['last_name'] . "]",
+                $this->getInfo($user_id)['first_name'] . " " . $this->getInfo($user_id)['last_name'],
+                "[id{$user_id}|" . $this->getInfo($user_id)['first_name'] . " " . $this->getInfo($user_id)['last_name'] . "]",
+            ], $text
+        );
+    }
+    
+    public function getInfo(int $user_id, string $name_case = ""): array {
+        return $this->bot->api("users.get", ["user_ids" => $user_id, "name_case" => $name_case])[0];
     }
 }
