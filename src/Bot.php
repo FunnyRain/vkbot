1a0c461727f8be152e7fe11ec1a760eb03c155e9
PechenkaPM <47981825+PechenkaPM@users.noreply.github.com>
2021-01-15 17:37:05 +0200

    Update Bot.php
    Не нужно удалять, оно перезапишеться

--- src/Bot.php
+++ src/Bot.php
@@ -141,7 +141,6 @@ class Bot {
             if (count($updates) == 0) continue;
             
             foreach ($updates as $key => $update) {
-                unset($this->vkdata);
                 $object = $updates[$key]['object'];
                 $this->vkdata = (isset($object['message'])) ? $object['message'] + $object['client_info'] + ['type' => $updates[$key]['type']]
                     : $object + ['type' => $updates[$key]['type']];
